26ecc5687fa4d23803934844de778337cea523d3
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-03-13 14:02:10 +0200

    test

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,47 +1,17 @@
 #!/usr/bin/python3
-
-"""
-Square class
-"""
+""" Square class module """
 
 
 class square():
-    """ Documentation """
-    __width = 0
-    __height = 0
+    """ Square class """
+    width = 0
+    height = 0
 
-    def __init__(self, width = 0, height = 0):
+    
+    def __init__(self, *args, **kwargs):
         """ Constructor """
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """ Getter """
-        return self.__width
-    
-    @width.setter
-    def width(self, value):
-        """ Setter """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-    
-    @property
-    def height(self):
-        """ Getter """
-        return self.__height
-    
-    @height.setter
-    def height(self, value):
-        """ Setter """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -55,8 +25,8 @@ class square():
         """ String representation of the square """
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
+    """ main """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
